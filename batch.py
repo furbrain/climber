38dfbe8a463ba3e9024d15df32f164f178dad2e2
Phil Underwood <beardydoc@gmail.com>
2020-12-21 02:02:25 +0000

    Add some validation to batch info

--- batch.py
+++ batch.py
@@ -13,6 +13,32 @@ def wx2pydt(ctrl: wx.adv.DatePickerCtrl):
     return datetime.date.fromtimestamp(dt.GetTicks())
 
 
+class BatchValidator(wx.Validator):
+    def Validate(self, parent: "gui.GetUploadData"):
+        print("Validator called")
+        clinic_date = wx2pydt(self.GetWindow())
+        use_by_date = wx2pydt(parent.use_by_date)
+        expiry_date = wx2pydt(parent.expiry_date)
+        if clinic_date > use_by_date:
+            wx.MessageBox("Clinic date must be on or before use by date")
+            return False
+        if clinic_date > expiry_date:
+            wx.MessageBox("Clinic date must be on or before use by date")
+            return False
+        if clinic_date > datetime.date.today():
+            wx.MessageBox("Clinic date must not be in the future")
+            return False
+        return True
+
+    def Clone(self):
+        return BatchValidator()
+
+    def TransferFromWindow(self):
+        return True
+
+    def TransferToWindow(self):
+        return True
+
 class BatchInfo:
     def __init__(self, dlg: "gui.GetUploadData"):
         self.clinic_date = wx2pydt(dlg.clinic_date)
